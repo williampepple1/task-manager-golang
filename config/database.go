f25393e15e951015a34cefc2f4e75b0a485d2cce
William Pepple <williampepple1@gmail.com>
2023-10-11 12:59:18 +0100

    App containerized

--- config/database.go
+++ config/database.go
@@ -8,7 +8,7 @@ import (
 // InitDB initializes and returns a connection to the database
 func InitDB() (*gorm.DB, error) {
 	// The connection string should be retrieved from environment variables or a secure config for production apps!
-	const connStr = "host=localhost user=postgres dbname=taskmanager sslmode=disable password=postgres"
+	const connStr = "host=localhost user=postgres dbname=taskmanager-db sslmode=disable password=postgres"
 
 	db, err := gorm.Open("postgres", connStr)
 	if err != nil {
